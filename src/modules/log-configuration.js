61116f151d11706a2bc8257ea14655cd4a21b6f6
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-06-28 08:50:38 -0300

    sadasdasd

--- src/modules/log-configuration.js
+++ src/modules/log-configuration.js
@@ -1,12 +1,4 @@
 import winston from "winston";
-/*
-    0: error
-    1: warn
-    2: info
-    3: verbose
-    4: debug
-    5: silly
-*/
 const logConfiguration = {
     transports: [
         new winston.transports.Console({
@@ -14,7 +6,6 @@ const logConfiguration = {
         }),
         new winston.transports.File({
             level: 'info',
-            // Create the log directory if it does not exist
             filename: 'logs/example.log'
         })
     ]
--- src/modules/log-helper.js
+++ src/modules/log-helper.js
@@ -5,4 +5,3 @@ import winston from "winston";
 const logger = winston.createLogger(logConfiguration);
 
 module.exports = logger;
-//logger.error("Hello, Winston logger, the first error!");
